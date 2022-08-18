11ced88d64c8fd45f7a80f31ed36dd9e1cb6f1b5
risheetadrevol <risheeta@drevol.com>
2022-08-18 17:46:12 +0530

    first commit

---
+++ lib/spin_wheel.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:http/http.dart';
+
+class ExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fortune Wheel Example',
+      home: ExamplePage(),
+    );
+  }
+}
+
+class ExamplePage extends StatefulWidget {
+  @override
+  _ExamplePageState createState() => _ExamplePageState();
+}
+
+class _ExamplePageState extends State<ExamplePage> {
+  StreamController<int> selected = StreamController<int>();
+
+  @override
+  void dispose() {
+    selected.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final items = <String>[
+      'Grogu',
+      'Mace Windu',
+      'Obi-Wan Kenobi',
+      'Han Solo',
+      'Luke Skywalker',
+      'Darth Vader',
+      'Yoda',
+      'Ahsoka Tano',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Fortune Wheel'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            selected.add(
+              Fortune.randomInt(0, items.length),
+            );
+          });
+        },
+        child: Column(
+          children: [
+            Expanded(
+              child: FortuneWheel(
+                indicators: [FortuneIndicator(child: Text("data"))],
+                selected: selected.stream,
+                onFling: () {
+                  print("dfctrhb");
+                },
+                items: [
+                  for (var it in items)
+                    FortuneItem(
+                        style: FortuneItemStyle(color: Colors.yellow),
+                        child: Text(it)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  getNotificationRequest(
+      {required String token, required String title, required String body}) {
+    String key = "";
+    post(Uri.parse("https://fcm.googleapis.com/fcm/send"), headers: {
+      HttpHeaders.authorizationHeader: "key=",
+      HttpHeaders.contentTypeHeader: "application/json",
+    }, body: {
+      "to": token,
+      "data": {"body": body, "title": title}
+    }).then((value) {
+      print("value");
+    });
+  }
+}
+// curl -X POST \
+//   https://fcm.googleapis.com/fcm/send \
+//   -H 'Authorization: key=AAAAG-oB4hk:APA91bFUilE6XqGzlqtr-M-LRl1JisWgEaSDfMZfHuJq3fs7IuvwhjoGM50i0YgU_qayJA8FKk15Uvkuo7SQtQlVt4qdcrrhvnfZyk_8zRGAskzalFUjr2nA2P_2QYNTfK6X8GbY0rni' \
+//   -H 'Content-Type: application/json' \
+//   -H 'Postman-Token: c8af5355-dbf2-4762-9b37-a6b89484cf07' \
+//   -H 'cache-control: no-cache' \
+//   -d '{
+//     "to": "ey_Bl_xs-8o:APA91bERoA5mXVfkzvV6I1I8r1rDXzPjq610twte8SUpsKyCuiz3itcIBgJ7MyGRkjmymhfsceYDV9Ck-__ObFbf0Guy-P_Pa5110vS0Z6cXBH2ThnnPVCg-img4lAEDfRE5I9gd849d",
+//     "data":{
+//         "body":"Test Notification !!!",
+//         "title":"Test Title !!!"
+//     }
+
+// }'
